941420cb414734d56ff6f58107cd4b25aedb1e96
M-4282 <127205349+M-4282@users.noreply.github.com>
2023-03-15 10:13:17 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+let hand = 0
+input.onSound(DetectedSound.Loud, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (hand == 3) {
+        basic.showLeds(`
+            # # . . .
+            # # # . #
+            . . . # .
+            # # # . #
+            # # . . .
+            `)
+    }
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
